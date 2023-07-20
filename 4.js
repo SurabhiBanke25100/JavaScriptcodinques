5717dafb27eb8e9e91f901158a0c0fecf3364380
su25100 <surbhibanke2599@gmail.com>
2023-07-20 23:58:43 +0530

    sum of no

---
+++ 4.js
@@ -0,0 +1,11 @@
+// program to add two no  usingfunction
+
+
+function add(a, b) {
+    return a + b;
+}
+
+
+let n = prompt("enter number1=");
+let n1 = prompt("enter number2=");
+console.log("sum=", add(n, n1));
