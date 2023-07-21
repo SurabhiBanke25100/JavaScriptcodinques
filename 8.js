166d1d6e7631d9984165d7571065987440a52e2e
su25100 <surbhibanke2599@gmail.com>
2023-07-22 00:33:50 +0530

    sort array in  desc orde

--- 8.js
+++ 8.js
@@ -1,11 +1,11 @@
-//Write a function to  sorted array in ascending order.
+//Write a function to  sorted array in descending order.
 function ascsort(inputArray) {
 
     var temp;
     for (let i = 0; i < inputArray.length; i++) {
         for (let j = i + 1; j < inputArray.length; j++) {
 
-            if (inputArray[i] > inputArray[j]) {
+            if (inputArray[i] < inputArray[j]) {
                 temp = inputArray[i];
                 inputArray[i] = inputArray[j];
                 inputArray[j] = temp;
@@ -21,4 +21,4 @@ let userInput = prompt("enter array =");
 let inputArray = userInput.split("").map(Number); //use the map():-> while spliting the input string  to convert the elements to numbers for comparison
 
 let sortedArray = ascsort(inputArray);
-console.log("Sorted array in ascending order:", sortedArray);
+console.log("Sorted array in descending order:", sortedArray);
