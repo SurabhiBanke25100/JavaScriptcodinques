f199905b2d867da7b2eb60125e998442500c6b21
Su25100 <surbhibanke2599@gmail.com>
2025-08-20 19:13:19 +0530

    learnningjs

---
+++ conversion.js
@@ -0,0 +1,48 @@
+let valuenumber = "25";
+//console.log(typeof valuenumber); // Output: string
+
+let numbervalue = Number(valuenumber);
+//console.log(typeof numbervalue); // Output: number
+
+let stringvalue = "";
+
+let convertboolean = Boolean(stringvalue);
+//console.log(convertboolean); // Output: false
+
+//if booleanvalue = "helloteam67868$234" 
+//thn output will be true
+
+let undefvalue = undefined;
+
+let nummvalue =Number(undefvalue);
+//console.log(nummvalue); // Output: NaN
+
+
+let nullvalue = null;
+
+let valueinNumber = Number(nullvalue);
+//console.log(valueinNumber); // Output: 0
+
+let srtingvalue = "hello team";
+let valueinNumber2 = Number(srtingvalue);
+//console.log(valueinNumber2); // Output: NaN
+
+let numvalue = 78
+let valueinString = String(numvalue);
+//console.log(typeof+"\n"+ "\nvalueinString:" + valueinString); // Output: "78"
+
+let addvalue = 30 - "40";
+//console.log(addvalue); // Output: -10
+
+console.log("2" == 2) //output: true
+console.log("2" === 2) //output: false
+
+
+
+
+
+
+
+
+
+
---
+++ datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //strict mode denotes the
+
+//Number
+//BigInt
+//String
+//null
+//undefined
+//Boolean
+//Symbol
+
+//null vs defined:
+// undefined is a type of variable that has been declared but not assigned a value, while null is an assignment value that represents no value.
+//ex- cart is "undefined" meaning it has not got value from the server yet, while cart returned null means the cart exists but remains empty.
+
